94ec2d50169923ee1443ea4e5c7aaba10e5c3994
whoismd <48346450+whoismd@users.noreply.github.com>
2023-01-13 14:16:05 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,30 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 from django.shortcuts import render
-from .models import Product
+from .models import ScrapedProduct
+from datetime import datetime, timedelta
 
-def scrape(request, url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    title = soup.find('span', class_='_35KyD6').get_text()
-    description = soup.find('div', class_='_3la3Fn').get_text()
-    price = soup.find('div', class_='_30jeq3 _1_WHN1').get_text()
-    mobile_number = soup.find('div', class_='_2aK_gu').get_text()
-    size = soup.find('div', class_='_2xg6Ul').get_text()
-    category = soup.find('div', class_='_2kHMtA').get_text()
-    images = soup.find_all('img', class_='_2VeolH _3I5S6S')
-    images = [img['src'] for img in images]
+class ScraperView(View):
+    def get(self, request, url):
+        product = ScrapedProduct.objects.filter(url=url).first()
+        if product and (datetime.now() - product.scraped_at).days < 7:
+            return render(request, 'scraper/scraped.html', {'product': product})
+        else:
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            title = soup.find('span', class_='product-title').get_text()
+            description = soup.find('div', class_='product-description').get_text()
+            price = soup.find('div', class_='product-price').get_text()
+            mobile_number = soup.find('div', class_='product-mobile-number').get_text()
+            size = soup.find('div', class_='product-size').get_text()
+            category = soup.find('div', class_='product-category').get_text()
+            images = soup.find_all('img', class_='product-image')
+            images = [img['src'] for img in images]
+            if product:
+                product.title = title
+                product.description = description
+                product.price = price
+                product.mobile_number = mobile_number
+                product.size = size
+                product.category = category
+                product.images = images
+                product.scraped_at = datetime.now()
+                product.save()
+            else:
+                product = ScrapedProduct(
+                    url=url,
+                    title=title,
+                    description=description,
+                    price=price,
+                    mobile_number=mobile_number,
+                    size=size,
+                    category=category,
+                    images=images,
+                    scraped_at=datetime.now()
+                )
+                product.save()
+            return render(request, 'scraper/scraped.html', {'product': product})
 
-    product = Product(
-        url=url,
-        title=title,
-        description=description,
-        price=price,
-        mobile_number=mobile_number,
-        size=size,
-        category=category,
-        images=images
-    )
-    product.save()
-
-    return render(request, 'scraper/scraped.html', {'product': product})
